44feedf906be1978d22b8db549c47774eca2a0ac
SmartWalle <smartwalle@gmail.com>
2017-10-20 10:10:18 +0800

    公开 DoRequest 方法

--- wxpay.go
+++ wxpay.go
@@ -1,17 +1,18 @@
 package wxpay
 
 import (
+	"encoding/xml"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"encoding/xml"
 )
 
 type WXPay struct {
-	appId  string
-	apiKey string
-	mchId  string
-	Client *http.Client
+	appId     string
+	apiKey    string
+	mchId     string
+	Client    *http.Client
+	NotifyURL string
 }
 
 func New(appId, apiKey, mchId string) (client *WXPay) {
@@ -23,10 +24,15 @@ func New(appId, apiKey, mchId string) (client *WXPay) {
 	return client
 }
 
-func (this *WXPay)doRequest(method, url string, param map[string]interface{}, results interface{}) (err error) {
+func (this *WXPay) doRequest(method, url string, param map[string]interface{}, results interface{}) (err error) {
 	param["appid"] = this.appId
 	param["mch_id"] = this.mchId
 	param["nonce_str"] = getNonceStr()
+	if _, ok := param["notify_url"]; ok == false {
+		if len(this.NotifyURL) {
+			param["notify_url"] = this.NotifyURL
+		}
+	}
 
 	var sign = signMD5(param, this.apiKey)
 	param["sign"] = sign
@@ -54,6 +60,6 @@ func (this *WXPay)doRequest(method, url string, param map[string]interface{}, re
 	return err
 }
 
-func (this *WXPay)DoRequest(method, url string, param map[string]interface{}, results interface{}) (err error) {
+func (this *WXPay) DoRequest(method, url string, param map[string]interface{}, results interface{}) (err error) {
 	return this.doRequest(method, url, param, results)
 }
