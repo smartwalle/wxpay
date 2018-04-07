0b847999ac4dcf5561b3d7526763a2e99ed8ed25
Yangfeng <smartwalle@gmail.com>
2018-04-07 19:22:01 +0800

    update

--- order_test.go
+++ order_test.go
@@ -10,23 +10,23 @@ func TestWXPay_UnifiedOrder(t *testing.T) {
 	var p = UnifiedOrderParam{}
 	p.Body = "test product"
 	p.NotifyURL = "http://www.test.com"
-	p.TradeType = K_TRADE_TYPE_APP
+	p.TradeType = K_TRADE_TYPE_NATIVE
 	p.SpbillCreateIP = "202.105.107.18"
 	p.TotalFee = 101
-	p.OutTradeNo = "test-111111"
+	p.OutTradeNo = "test-11111112"
 
-	//result, err := client.UnifiedOrder(p)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	t.Fatal(err)
-	//}
-	//fmt.Println(result.PrepayId, result.CodeURL)
+	result, err := client.UnifiedOrder(p)
+	if err != nil {
+		fmt.Println(err)
+		t.Fatal(err)
+	}
+	fmt.Println(result.PrepayId, result.CodeURL)
 }
 
 func TestWXPay_OrderQuery(t *testing.T) {
 	fmt.Println("========== OrderQuery ==========")
 	var p = OrderQueryParam{}
-	p.OutTradeNo = "test-111111"
+	p.OutTradeNo = "test-11111112"
 
 	result, err := client.OrderQuery(p)
 	if err != nil {
