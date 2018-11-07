448af4a63f397670ed62acad0eb23fa98596d9f4
SmartWalle <smartwalle@gmail.com>
2018-11-07 17:01:22 +0800

    format

--- wxpay_test.go
+++ wxpay_test.go
@@ -8,6 +8,7 @@ import (
 var client *WXPay
 
 func TestMain(m *testing.M) {
+	//client = New("wx7224e0425e3b8655", "hejzqb1aajazfme7v2zs8e349t8f135f", "1491203582", true)
 	client = New("wx20fa044851046bbf", "1v4h5g4s8u1x25tf451d025e10geagf2", "1299730801", false)
 	os.Exit(m.Run())
 }
